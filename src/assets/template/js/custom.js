102755c5bcd256ac4be4bd5c6ac514bdeb0ee341
Mehdi Jarrar <mehdi.jarrar@proxym-it.com>
2021-07-03 18:01:30 +0100

    3/07/2021

--- src/assets/template/js/custom.js
+++ src/assets/template/js/custom.js
@@ -98,6 +98,43 @@ jQuery(function($){
       ]
     }); 
 
+    jQuery('.aa-popular-slider2').slick({
+      dots: false,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 6,
+      slidesToScroll: 6,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    }); 
+
+  
   
   /* ----------------------------------------------------------- */
   /*  5. FEATURED PRODUCT SLIDER (SLICK SLIDER)
